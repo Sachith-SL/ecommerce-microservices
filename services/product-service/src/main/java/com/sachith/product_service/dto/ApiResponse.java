5a26e9d9b107d11e648f45c5b1e0cd3e1ad72de6
Sachith-SL <itsmesachith@gmail.com>
2026-03-10 21:47:26 +0530

    add layers stucture and base responce and global exception handle

---
+++ services/product-service/src/main/java/com/sachith/product_service/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.sachith.product_service.dto;
+
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
