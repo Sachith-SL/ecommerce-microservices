b9a49532218968180a6d76ad58ca55343e1e8fac
Sachith-SL <itsmesachith@gmail.com>
2026-03-14 07:44:30 +0530

    Add RateLimiterConfig class for request rate limiting; include user key resolver bean

---
+++ infrastructure/api-gateway/src/main/java/com/scn/api_gateway/config/RateLimiterConfig.java
@@ -0,0 +1,20 @@
+package com.scn.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest()
+                        .getRemoteAddress()
+                        .getAddress()
+                        .getHostAddress()
+        );
+    }
+}
